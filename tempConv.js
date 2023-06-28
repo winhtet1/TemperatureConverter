8528078a60fec4deceddb246d410e42719874ffa
Win Htet Aung <51900376+winhtet1@users.noreply.github.com>
2023-06-28 10:22:36 +0630

    Create tempConv.js

---
+++ tempConv.js
@@ -0,0 +1,33 @@
+document.getElementById("submitBtn").onclick = function(){
+
+    let temp;
+
+    if(document.getElementById("cBtn").checked){
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°C";
+    } 
+    else if (document.getElementById("fBtn").checked){
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toFahrenheit(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°F";
+    } 
+    else {
+        document.getElementById("tempLabel").innerHTML = "Select a unit.";
+    }
+
+
+}
+temp = toFahrenheit(temp);
+console.log(temp);
+
+function toCelsius(temp){
+    return (temp - 32) * (5/9);
+}  
+
+function toFahrenheit(temp){
+    return (temp * 9 / 5 + 32 );
+}
+
